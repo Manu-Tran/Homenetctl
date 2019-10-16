6eb816144e58121d3a2bccbb308a360c1870a6cc
Manu-Tran <emmanuel.tran@gmail.com>
2019-10-16 16:34:30 +0200

    EHN: Adding self-signed certificate generation

--- src/include/certificate.h
+++ src/include/certificate.h
@@ -5,18 +5,22 @@
 #ifndef HOMENETCTL_CERTIFICATE_H
 #define HOMENETCTL_CERTIFICATE_H
 
+#include <string>
+#include <cstdio>
+#include <iostream>
 
-class certificate {
+#include <openssl/pem.h>
+#include <openssl/x509.h>
+
+class Certificate {
 
 private:
     long int mSeqnum = 0;
-    int mCertificate; //to be changed for the right type
+    X509* mCertificate; //to be changed for the right type
 
 public:
-
-    certificate();
-    void generateCertificate(); //should return a certif afterwards
-    bool checkCertfification(int publicKey);
+    Certificate(char* nom, EVP_PKEY * pubKey, int validityDays);
+    bool checkCertfification( EVP_PKEY * publicKey);
 
 };
 
--- src/sources/certificate.cpp
+++ src/sources/certificate.cpp
@@ -4,13 +4,42 @@
 
 #include "certificate.h"
 
-certificate::certificate()=default;
-void certificate::generateCertificate()
-{
-    //here you can generate the certificate using public key from other party
-};
 
-bool certificate::checkCertfification(int publicKey)
+/// Generate a new certificate
+/// @param name
+/// @param pubkey
+/// @param validityDate
+/// @return a new certificate
+Certificate::Certificate(char *nom, EVP_PKEY *pubKey, int validityDays){
+    X509 *x509 = X509_new();
+    if(!x509) {
+        std::cerr << "Unable to create X509 structure." << std::endl;
+    }
+    /* Set the date limits of the certificate */
+    X509_gmtime_adj(X509_get_notBefore(x509), 0);
+    X509_gmtime_adj(X509_get_notAfter(x509), validityDays*24*60*60);
+
+    /* Set the public key of the certificate */
+    X509_set_pubkey(x509, pubKey);
+
+    /* We want to copy the subject name to the issuer name. */
+    X509_NAME * name = X509_get_subject_name(x509);
+
+
+    X509_NAME_add_entry_by_txt(name, "CN", MBSTRING_ASC, (unsigned char *)nom, -1, -1, 0);
+    /* Set the issuer name. */
+    X509_set_issuer_name(x509, name);
+
+    /* Sign the certificate with our key. */
+    if(!X509_sign(x509, pubKey, EVP_sha1()))
+    {
+        std::cerr << "Error signing certificate." << std::endl;
+        X509_free(x509);
+    }
+}
+
+bool Certificate::checkCertfification(EVP_PKEY * publicKey)
 {
+    return false;
     //check signature using parameter key
 };

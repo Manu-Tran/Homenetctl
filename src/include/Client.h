8a902755915360ccbc7a628f888ef32574650dbb
Abadacor <romainhe.brisse@gmail.com>
2019-11-02 10:47:55 +0100

    New class: Client (inherits from socket)

---
+++ src/include/Client.h
@@ -0,0 +1,31 @@
+//
+// Created by romain on 02/11/2019.
+//
+
+#ifndef HOMENETCTL_CLIENT_H
+#define HOMENETCTL_CLIENT_H
+
+#include <netinet/in.h>
+#include "Socket.h"
+
+#define PORT 1234
+
+class Client : public Socket {
+
+private:
+
+    int mSocket = 0;
+    struct sockaddr_in mServerAddress;
+
+public:
+
+    Client();
+
+    //has to be called inside the connect function to get the right IP address and port for the server
+    sockaddr_in getServerAddress();
+    bool connect();
+
+};
+
+
+#endif //HOMENETCTL_CLIENT_H
---
+++ src/sources/Client.cpp
@@ -0,0 +1,12 @@
+//
+// Created by romain on 02/11/2019.
+//
+
+#include "Client.h"
+
+Client::Client() {}
+
+bool Client::connect() {}
+
+sockaddr_in Client::getServerAddress() {}
+
